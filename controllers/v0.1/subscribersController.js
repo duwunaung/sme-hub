0bf59ae6e23df7cbbf82058d07e6c931ab86ea4e
TaukTaukLin <tauktauk51833@gmail.com>
2025-01-23 21:24:27 +0700

    Change-log by tauktauk on Jan 23rd - added login and logout

---
+++ controllers/v0.1/subscribersController.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+
+exports.login = (req, res) => {
+	if (req.method === 'POST') {
+		const { email, password } = req.body;
+		try {
+			axios.post(`${process.env.API_URL}/utils/login`, { email, password })
+				.then(response => {
+					if (response.data.success) {
+						req.session.token = response.data.data.token;
+						req.session.user = response.data.data.name;
+						res.redirect('/subscriber/home');
+					} else {
+						res.render('subscriber/login', { errorMessage: response.data.message });
+					}
+				})
+				.catch(error => {
+					res.render('subscriber/login', { errorMessage: error.response.data.message });
+				})
+		} catch (error) {
+			res.render('subscriber/login', { errorMessage: 'An error occurred during login.' });
+		}
+	} else {
+		res.render('subscriber/login', { errorMessage: null })
+	}
+
+}
+
+exports.logout = (req, res) => {
+	if (req.session) {
+		req.session.destroy(err => {
+			if (err) {
+				return res.redirect('/'); // Redirect to home on error
+			}
+			res.redirect('login'); // Redirect to login after successful logout
+		});
+	} else {
+		res.redirect('login'); // If no session, redirect to login
+	}
+}
