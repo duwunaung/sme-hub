e1f66b2b2cba325ab36b433fb22131b54ca10f47
TaukTaukLin <tauktauk51833@gmail.com>
2025-01-30 17:29:15 +0700

    Change-log by tauktauk on Jan 30th - added routes for category detail

--- routes/v0.1/suViewsRoute.js
+++ routes/v0.1/suViewsRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { login, logout, listExpenseCat, createExpenseCat, createIncomeCat, listIncomeCat , deleteIncomeCat, restoreIncomeCat , deleteExpenseCat, restoreExpenseCat , updateIncomeCat, updateExpenseCat, listIncomeTrans, listExpenseTrans , createIncomeTrans, createExpenseTrans , updateIncomeTrans, updateExpenseTrans} = require("../../controllers/v0.1/subscribersController")
+const { login, logout, listExpenseCat, createExpenseCat, createIncomeCat, listIncomeCat , deleteIncomeCat, restoreIncomeCat , deleteExpenseCat, restoreExpenseCat , updateIncomeCat, updateExpenseCat, listIncomeTrans, listExpenseTrans , createIncomeTrans, createExpenseTrans , updateIncomeTrans, updateExpenseTrans , detailIncomeTrans, detailExpenseTrans, detailIncomeCat, detailExpenseCat} = require("../../controllers/v0.1/subscribersController")
 
 const checkSubscriberSession = require('../../middlewares/viewSubscribers');
 const tmpSession = require('../../middlewares/tmp');
@@ -8,21 +8,25 @@ const router = express.Router();
 router.use('/login', login)
 router.use('/logout', logout)
 
+router.use('/transaction/income/detail/:id', checkSubscriberSession, detailIncomeTrans )
 router.use('/transaction/income/update/:id', checkSubscriberSession, updateIncomeTrans )
 router.use('/transaction/income/create', checkSubscriberSession, createIncomeTrans )
 router.use('/transaction/income', checkSubscriberSession, listIncomeTrans )
 
+router.use('/transaction/expense/detail/:id', checkSubscriberSession, detailExpenseTrans )
 router.use('/transaction/expense/update/:id', checkSubscriberSession, updateExpenseTrans )
 router.use('/transaction/expense/create', checkSubscriberSession, createExpenseTrans )
 router.use('/transaction/expense', checkSubscriberSession, listExpenseTrans )
 
 router.use('/category/income/create', checkSubscriberSession, createIncomeCat )
+router.use('/category/income/detail/:id', checkSubscriberSession, detailIncomeCat )
 router.use('/category/income/update/:id', checkSubscriberSession, updateIncomeCat )
 router.use('/category/income/delete/:id', checkSubscriberSession, deleteIncomeCat )
 router.use('/category/income/restore/:id', checkSubscriberSession, restoreIncomeCat )
 router.use('/category/income', checkSubscriberSession, listIncomeCat )
 
 router.use('/category/expense/create', checkSubscriberSession, createExpenseCat )
+router.use('/category/expense/detail/:id', checkSubscriberSession, detailExpenseCat )
 router.use('/category/expense/update/:id', checkSubscriberSession, updateExpenseCat )
 router.use('/category/expense/delete/:id', checkSubscriberSession, deleteExpenseCat )
 router.use('/category/expense/restore/:id', checkSubscriberSession, restoreExpenseCat )
