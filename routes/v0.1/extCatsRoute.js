65b944405566d8047ccf891948c6107adec494ef
TaukTaukLin <tauktauk51833@gmail.com>
2025-02-14 12:45:49 +0700

    Change-log by tauktauk on Feb 14th - added new routes getIncCat and getExpCat

--- routes/v0.1/extCatsRoute.js
+++ routes/v0.1/extCatsRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
-const { createExpenseCategory, listExpenseCategory, updateExpenseCategory, deleteExpenseCategory, createIncomeCategory, updateIncomeCategory, deleteIncomeCategory, listIncomeCategory, restoreIncomeCategory , restoreExpenseCategory , getIncomeCategory , getExpenseCategory , detailExpenseCategory, detailIncomeCategory, listIncCat, listExpCat, listAllCategories} = require('../../controllers/v0.1/extCatsController')
+const { getIncCat, getExpCat, createExpenseCategory, listExpenseCategory, updateExpenseCategory, deleteExpenseCategory, createIncomeCategory, updateIncomeCategory, deleteIncomeCategory, listIncomeCategory, restoreIncomeCategory , restoreExpenseCategory , getIncomeCategory , getExpenseCategory , detailExpenseCategory, detailIncomeCategory, listIncCat, listExpCat, listAllCategories} = require('../../controllers/v0.1/extCatsController')
 
 const router = express.Router();
 
 router.post('/expense/create', createExpenseCategory);
 router.get('/expense/list', listExpenseCategory);
 router.get('/expense/detail/:id', detailExpenseCategory);
+router.get('/expense/category/:name', getExpCat);
 router.get('/expense/:id', getExpenseCategory);
 router.put('/expense/:id', updateExpenseCategory);
 router.delete('/expense/:id', deleteExpenseCategory);
@@ -17,10 +18,11 @@ router.get('/expense', listExpCat);
 router.post('/income/create', createIncomeCategory);
 router.get('/income/list', listIncomeCategory);
 router.get('/income/detail/:id', detailIncomeCategory);
+router.put('/income/restore/:id', restoreIncomeCategory);
+router.get('/income/category/:name', getIncCat);
 router.get('/income/:id', getIncomeCategory);
 router.put('/income/:id', updateIncomeCategory);
 router.delete('/income/:id', deleteIncomeCategory);
-router.put('/income/restore/:id', restoreIncomeCategory);
 router.get('/income', listIncCat);
 
 router.get('/', listAllCategories);
