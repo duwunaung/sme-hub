8591e45dc86a767baddae45e986fa8176dec0f56
TaukTaukLin <tauktauk51833@gmail.com>
2025-02-19 23:55:22 +0700

    Change-log by tauktauk on Feb 19th - created a route path for managing salesperson

---
+++ routes/v0.1/extSalespersonRoute.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { deleteSalesperson, restoreSalesperson, getSalespersonName, getSalesperson, getSalespersonList, createSalesperson , updateSalesperson } = require('../../controllers/v0.1/subOrgController')
+
+const router = express.Router()
+
+router.get('/list/:name', getSalespersonName)
+router.get('/list', getSalespersonList)
+router.put('/update/:id', updateSalesperson)
+router.post('/create', createSalesperson)
+router.delete('/delete/:id', deleteSalesperson)
+router.put('/restore/:id', restoreSalesperson)
+router.get('/:id', getSalesperson)
+
+
+module.exports = router
