a8ce4723fb88811a23af76f9795fbbc2007da642
Rick <tauktauk51833@gmail.com>
2025-04-14 20:10:15 +0700

    Change-log by tauktauk on Apr 14 - stored the value of access or deny in cache and made it refresh for every 1 minute

--- middlewares/subaccess.js
+++ middlewares/subaccess.js
@@ -1,17 +1,30 @@
-const db_connection = require('../utils/connection')
+const db_connection = require('../utils/connection');
+require('dotenv').config()
+let isSubscriberAccessEnabled = false;
 
-const subAccess = (req, res, next) => {
+function refreshAccessState() {
   db_connection.query("SELECT access FROM subaccess WHERE id = ?", [1], (err, result) => {
     if (err) {
-      console.error("Database error:", err);
-      return res.render('subscriber/access-deny');
+      console.error("Error refreshing access state:", err);
+      return;
     }
-    if (result && result.length > 0 && result[0].access) {
-      next();
-    } else {
-      res.render('subscriber/access-deny');
-    }
+    
+    isSubscriberAccessEnabled = result && result.length > 0 && result[0].access === 1;
+    console.log(`Subscriber access state refreshed: ${isSubscriberAccessEnabled ? 'enabled' : 'disabled'}`);
   });
 }
 
-module.exports = subAccess
+refreshAccessState();
+const minuteNum = process.env.REFRESH_INTERVAL || 1
+const REFRESH_INTERVAL = minuteNum * 60 * 1000;
+setInterval(refreshAccessState, REFRESH_INTERVAL);
+
+const subAccess = (req, res, next) => {
+  if (isSubscriberAccessEnabled) {
+    next();
+  } else {
+    res.render('subscriber/access-deny');
+  }
+};
+
+module.exports = subAccess;
