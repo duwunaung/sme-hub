10e034ae1b37fd4bd8516e913ac24c5912f0b6b9
Rick <tauktauk51833@gmail.com>
2025-05-29 19:17:34 +0700

    Change-log by tauktauk on May 29 - added routes for fetching data for the report by salesperson

--- routes/v0.1/extSalespersonRoute.js
+++ routes/v0.1/extSalespersonRoute.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const { getSalespersonListAll,deleteSalesperson, restoreSalesperson, getSalespersonName, getSalesperson, getSalespersonList, createSalesperson , updateSalesperson } = require('../../controllers/v0.1/subOrgController')
+const {getDetailsReportTrans, getDetailsReport, getDetailReport, getSalesListAll, getSalespersonListAll,deleteSalesperson, restoreSalesperson, getSalespersonName, getSalesperson, getSalespersonList, createSalesperson , updateSalesperson } = require('../../controllers/v0.1/subOrgController')
 
 const router = express.Router()
 
+router.get('/detailreporttrans/:id', getDetailsReportTrans)
+router.get('/detailreports/:id', getDetailsReport)
+router.get('/detailreport/:id', getDetailReport)
 router.get('/list/:name', getSalespersonName)
 router.get('/all', getSalespersonListAll)
+router.get('/listall', getSalesListAll)
 router.get('/list', getSalespersonList)
 router.put('/update/:id', updateSalesperson)
 router.post('/create', createSalesperson)
